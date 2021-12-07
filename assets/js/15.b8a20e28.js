a84bff0367f02ed8875f6caeac358aea2f024f0a
Jono <developer@jono.fyi>
2021-12-07 09:40:33 -0800

    deploy

---
+++ assets/js/15.b8a20e28.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[15],{432:function(t,e,s){"use strict";s.r(e);var n=s(53),r=Object(n.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("redirect-page",{attrs:{src:"/docs/two"}})],1)}),[],!1,null,null,null);e.default=r.exports}}]);
